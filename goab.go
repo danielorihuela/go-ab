429274d443b810fde086b2d8c38f79f4e812899c
dani <danielorihuelarodriguez@gmail.com>
2021-03-20 09:28:58 +0100

    feat: keep alive parameter

--- goab.go
+++ goab.go
@@ -7,9 +7,9 @@ import (
 	"os"
 )
 
-func sendRequest(id int, requestId int, testUrl string) int {
+func sendRequest(client *http.Client, id int, requestId int, testUrl string) int {
 	log.Println("Worker", id, "is launching request", requestId)
-	_, err := http.Get(testUrl)
+	_, err := client.Get(testUrl)
 	log.Println("Worker", id, "finished request", requestId)
 	if err != nil {
 		return 0
@@ -20,6 +20,7 @@ func sendRequest(id int, requestId int, testUrl string) int {
 func main() {
 	numberRequestsPtr := flag.Int("n", 1, "Number of requests to make")
 	numberConcurrentConnectionsPtr := flag.Int("c", 1, "Number of concurrent connections")
+	keepAlivePtr := flag.Bool("k", false, "Activate keep alive HTTP feature")
 	flag.Parse()
 
 	testUrl := os.Args[len(os.Args)-1]
@@ -27,14 +28,17 @@ func main() {
 	log.Println("Url to test =", testUrl)
 	log.Println("Number of requests =", *numberRequestsPtr)
 	log.Println("Concurrent requests =", *numberConcurrentConnectionsPtr)
+	log.Println("Keep Alive HTTP is activated =", *keepAlivePtr)
 
 	requests := make(chan int)
 	results := make(chan int, *numberRequestsPtr)
 
 	for connectionId := 0; connectionId < *numberConcurrentConnectionsPtr; connectionId++ {
+		transport := &http.Transport{DisableKeepAlives: !*keepAlivePtr}
+		client := &http.Client{Transport: transport}
 		go func(connectionId int) {
 			for requestId := range requests {
-				results <- sendRequest(connectionId, requestId, testUrl)
+				results <- sendRequest(client, connectionId, requestId, testUrl)
 			}
 		}(connectionId)
 	}
